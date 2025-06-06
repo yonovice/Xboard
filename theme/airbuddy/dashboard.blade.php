814da7a52b76f60e23d3cd5da84ff6246375ff80
yonovice <yonovice258@gmail.com>
2025-06-06 09:54:51 +0800

    feat: Add SGEpay payment and airbuddy theme

---
+++ theme/airbuddy/dashboard.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>AirBuddy Rocket</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link
+      rel="icon"
+      type="image/svg+xml"
+      href="/theme/{{$theme}}/assets/C4MjgjTg.svg"
+    />
+    <link
+      rel="preload"
+      href="/theme/{{$theme}}/assets/CtaAdBwu.json"
+      as="fetch"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.font.im/css?family=Fredoka+One|Rubik:500"
+      crossorigin=""
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Rubik:wght@300..900&amp;display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@600&amp;display=swap"
+      rel="stylesheet"
+    />
+    <script
+      src="https://www.google.com/recaptcha/api.js?render=explicit&amp;onload=onReCAPTCHALoaded"
+      async=""
+      defer="defer"
+    ></script>
+    <link
+      rel="preload"
+      href="/theme/{{$theme}}/assets/CtaAdBwu.json"
+      as="fetch"
+    />
+    <script id="config" src="/theme/{{$theme}}/config.js"></script>
+    <script>
+      window.__deploy_route_base__ = '/theme/{{$theme}}';
+      window.CONFIG.theme.primary = '{{$theme_config['primary']}}'
+      window.CONFIG.theme.mode = '{{$theme_config['theme_mode']}}'
+      window.CONFIG.page.layout = '{{$theme_config['layout']}}';
+    </script>
+    <meta name="version" content="0.0.0-5e6364e3" />
+    <script
+      type="module"
+      crossorigin=""
+      src="/theme/{{$theme}}/assets/index-Cl98UmfH.js"
+    ></script>
+    <link
+      rel="stylesheet"
+      crossorigin=""
+      href="/theme/{{$theme}}/assets/BhOH5b9a.css"
+    />
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
